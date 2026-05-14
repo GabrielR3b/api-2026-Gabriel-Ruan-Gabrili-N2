bce21faafd10fc0d72223d1ec7829dce4d6859f4
GabrielR3b <GabrielR3b@github.com>
2026-05-14 11:13:41 -0300

    Atividade até o passo 4

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import path from "node:path"
 import express from "express"
 import { router as apiRouter } from "./routes/api.routes.js";
 import { mdebug } from "./middlewares/debug.middleware.js";
@@ -12,6 +13,47 @@ app.use(express.json()); // Middleware global para lidar com JSON
 app.use(mdebug);
 
 app.use("/api", apiRouter);
+
+
+
+// Configurações do EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "views")); // Node.js 20.11+
+
+// Middlewares
+app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.json()); // Importante para rotas REST
+
+app.get("/test", (req, res) => {
+    res.json({message:'alive'}) // Renderiza o esqueleto da página
+});
+
+// --- ROTA DE VIEW (EJS) ---
+
+app.get("/test", (req, res) => {
+    res.json({message:'ok!'}) // Renderiza o esqueleto da página
+});
+
+
+app.get("/home", (req, res) => {
+    res.render("home"); // Renderiza o esqueleto da página
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/products", (req, res) => {
+  const products = [
+    {id: 1, nome: "Teclado", disponivel: true},
+    {id: 2, nome: "Mouse", disponivel: true},
+    {id: 3, nome: "Monitor", disponivel: false},
+    {id: 4, nome: "Caixa de Som", disponivel: false},
+  ];
+  res.render("products", {listaProdutos: products});
+});
 
 // Após todas as rotas, há o tratamento de erro para uma rota inexistente
 // Se nenhuma das rotas funcionar, será enviado um erro 404 ao cliente (Pagina solicitada não encontrada).
